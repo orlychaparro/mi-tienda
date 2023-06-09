b938fc2572a2494faced345ce6cc95479a80097e
orlychaparro <orlychaparro@gmail.com>
2023-06-08 23:25:07 -0400

    productos de la tienda con componente para productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Product} from './models/product.model';
 import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 @Component({
@@ -7,8 +8,35 @@ import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  imgParent = '';
-  // https://www.w3schools.com/howto/img_avatar.png
+  imgParent = '';   // https://www.w3schools.com/howto/img_avatar.png
+  products : Product[] =
+    [
+      {
+        id: '1',
+        name: 'EL mejor juguete',
+        price: 565,
+        image: './assets/images/toy.jpg'
+      },
+      {
+        id: '2',
+        name: 'Bicicleta casi nueva',
+        price: 356,
+        image: './assets/images/bike.jpg'
+      },
+      {
+        id: '3',
+        name: 'Colleción de albumnes',
+        price: 34,
+        image: './assets/images/album.jpg'
+      },
+      {
+        id: '4',
+        name: 'Mis libros',
+        price: 23,
+        image: './assets/images/books.jpg'
+      },
+    ];
+
 
   onLoaded(img:string){
     console.log('log padre', img);

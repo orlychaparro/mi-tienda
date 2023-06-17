8bd860a4362fa19916f983076b603761e7cc6d85
orlychaparro <orlychaparro@gmail.com>
2023-06-16 23:47:46 -0400

    creaciòn de Servicios e inyecciòn de dependencias

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import {Product} from '../../models/product.model';
 
+import {StoreService} from '../../services/store.service';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -39,16 +41,19 @@ export class ProductsComponent implements OnInit {
       },
     ];
 
-    constructor(){ }
+    constructor(
+      private storeService : StoreService
+    ){
+      this.myShoppingCart = this.storeService.getShoppingCart();
+     }
 
     ngOnInit(): void {
 
     }
 
     onAddToShoppingCart(product:Product){
-      console.log(product);
-      this.myShoppingCart.push(product);
-      this.total = this.myShoppingCart.reduce((sum,item) => sum + item.price,0);
+      this.storeService.addProduct(product);
+      this.total = this.storeService.getTotal();
 
 
     }
